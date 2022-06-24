dbc82a5dfcb9fdd76af095810542c3a59c27fe19
Michal Kotowski <mzkotowski@gmail.com>
2022-06-24 15:31:36 +0200

    Where's the problem qq

--- server/index.js
+++ server/index.js
@@ -13,8 +13,6 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '../client/build')))
 }
 
-console.log(path.join(__dirname, '../client/build'))
-
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -37,4 +35,6 @@ app.get('*', (request, response) => {
 
 app.listen(port, () => {
 	console.log(`listening to port: ${port}`);
+}).on('error', err => {
+    console.log(err)
 })
